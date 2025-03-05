05675b91ea8941ccb48176319ae0b2c4f9ef0a59
coding-jhuunn <codingatjhunn@gmail.com>
2025-03-05 15:59:53 +0800

    initialize vue proj

---
+++ todoList/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import MyHeader from "./components/MyHeader.vue";
+</script>
+
+<template>
+  <div>
+    <MyHeader></MyHeader>
+  </div>
+</template>
---
+++ todoList/src/components/MyHeader.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+  <div>
+    <div>TodoList</div>
+    <div><a href="https://github.com/coding-jhuunn">coding-jhuunnn</a></div>
+  </div>
+</template>
