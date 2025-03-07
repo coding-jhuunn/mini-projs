de02153efde0e478914bfaf76a3d2757992fecbb
coding-jhuunn <codingatjhunn@gmail.com>
2025-03-07 16:12:42 +0800

    delete todList

--- todoList/src/components/MyHeader.vue
+++
@@ -1,8 +0,0 @@
-<script setup></script>
-
-<template>
-  <div class="flex justify-evenly">
-    <div class="border-b-1">TodoList</div>
-    <div class="border-b-1"><a href="https://github.com/coding-jhuunn">coding-jhuunnn</a></div>
-  </div>
-</template>
