39745a1072510f29feb260312f151c35e64c13ba
coding-jhuunn <codingatjhunn@gmail.com>
2025-03-26 16:16:36 +0800

    add fetchAPI vue ts

---
+++ FetchAPI-VUETS/src/App.vue
@@ -0,0 +1,14 @@
+<script setup="ts">
+// https://quotes-api-self.vercel.app/quote link of API
+
+import TitleCard from "./components/TitleCard.vue";
+import FetchApi from "./components/FetchApi.vue";
+const linkAPI = "https://quotes-api-self.vercel.app/quote";
+</script>
+
+<template>
+  <TitleCard></TitleCard>
+  <FetchApi :link="linkAPI" />
+</template>
+
+<style scoped></style>
