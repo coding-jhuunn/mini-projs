e9b5ce9eee1a4e9246079aaf1ad9db670aff163c
coding-jhuunn <codingatjhunn@gmail.com>
2025-04-01 14:30:22 +0800

    add nuxt practice

---
+++ nuxt-test/pages/about.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>about</h1>
+    {{ data }}
+  </div>
+</template>
+
+<script setup>
+const { data } = await useFetch("/api/exone?name=mario", {
+  method: "post",
+  body: {
+    age: 30,
+  },
+});
+</script>
